04a4139cddc321633d0d5d2b3fa1b82d8b897ece
Maciek C <mac.celinski@gmail.com>
2021-04-27 14:28:34 +0200

    Lecture 3 & 4 - Creating and accessing Theme Context - Functional Component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,17 +1,21 @@
+import { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 const BookList = () => {
-	return <div className="book-list">
-        <ul>
-            <li>
-                The Way of Kings
-            </li>
-            <li>
-                The Name of the Wind
-            </li>
-            <li>
-                The Wiseman Fear
-            </li>
-        </ul>
-    </div>;
+	const { isLightTheme, light, dark } = useContext(ThemeContext);
+	const theme = isLightTheme ? light : dark;
+
+	return (
+		<div
+			className="book-list"
+			style={{ color: theme.syntax, background: theme.bg }}
+		>
+			<ul>
+				<li style={{ background: theme.ui }}>The Way of Kings</li>
+				<li style={{ background: theme.ui }}>The Name of the Wind</li>
+				<li style={{ background: theme.ui }}>The Wiseman Fear</li>
+			</ul>
+		</div>
+	);
 };
 
 export default BookList;
