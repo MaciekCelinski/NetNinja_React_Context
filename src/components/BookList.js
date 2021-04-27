5d73e6c4610c57ead01294852241e48ad34ea729
Maciek C <mac.celinski@gmail.com>
2021-04-27 12:30:25 +0200

    Lecture_1

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+const BookList = () => {
+	return <div className="book-list">
+        <ul>
+            <li>
+                The Way of Kings
+            </li>
+            <li>
+                The Name of the Wind
+            </li>
+            <li>
+                The Wiseman Fear
+            </li>
+        </ul>
+    </div>;
+};
+
+export default BookList;
