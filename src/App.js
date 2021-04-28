9f15245ed64fcead2e08850ced2b46a2f18c6ef6
Maciek C <mac.celinski@gmail.com>
2021-04-28 14:25:55 +0200

    Lecture 9 - Hooks setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import BookList from "./components/BookList";
-import Navbar from "./components/Navbar";
-import ThemeToggle from "./components/ThemeToggle";
-import { AuthContextProvider } from "./contexts/AuthContext";
-import { ThemeContextProvider } from "./contexts/ThemeContext";
+import SongList from "./components/SongList";
 
 function App() {
 	return (
 		<div className="App">
-			<ThemeContextProvider>
-				<AuthContextProvider>
-					<Navbar />
-					<BookList />
-					<ThemeToggle />
-				</AuthContextProvider>
-			</ThemeContextProvider>
+			<SongList />
 		</div>
 	);
 }
---
+++ src/components/SongList.js
@@ -0,0 +1,12 @@
+const SongList = () => {
+	return (
+		<div className="song-list">
+			<ul>
+				<li>This wild darkness</li>
+				<li>Memory gospel</li>
+			</ul>
+		</div>
+	);
+};
+
+export default SongList;
